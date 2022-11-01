e8d05af14e221e79ce53acc2b2b4daa679247fc1
FatimahAlmarshdi <103451622+FatimahAlmarshdi@users.noreply.github.com>
2022-11-01 14:40:10 +0530

    Add files via upload
    In the idea of ​​the scroll, whenever I make a scroll from the y axis and the value of scrollY changes in the javascript
    As the design elements move, the first of them is the sky, then the moon, then the boat, etc.
    It also designs a switch between day and night based on the position of the moon in the design

---
+++ javaScript project/main.js
@@ -0,0 +1,17 @@
+let stars = document.getElementById('stars');
+let moon= document.getElementById('moon');
+let mountains3= document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let lernwithme = document.querySelector('.lernwithme');
+window.onscroll=function(){
+    let value= scrollY;
+    stars.style.left= value + 'px';
+    moon.style.top= value + 'px';
+    river.style.top= value  + 'px';
+    boat.style.top= value  + 'px';
+    boat.style.left= value  * 2 + 'px';
+
+
+}
